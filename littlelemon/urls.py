e13595bbfdd27c753069a01790da2c8931c26e2f
tooturntjroock <jonathanw1219@gmail.com>
2024-03-18 17:55:11 -0500

    api View

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
+    path('restaurant/menu/',include('restaurant.urls')),
+    
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from .models import Booking, Menu
+from rest_framework import generics, viewsets
+from .serializers import menuSerializer, bookingSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+    search_fields = ["category__title"]
+    ordering_fields = ["price", "inventory"]
+
+    
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = bookingSerializer
